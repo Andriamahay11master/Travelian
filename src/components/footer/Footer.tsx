a16b8bcbbf0529d012b0c6fd547b6bccd4b09721
Andriamahay11master <hirimanana@yahoo.fr>
2024-06-25 14:29:02 +0300

    modif strucuture footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,15 +1,87 @@
 import * as React from 'react';
 import './footer.scss';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Footer() {
     
     return (
         <footer className='footerPage'>
-            <div className="footerCopyright">
-                <div className="cntCopyright">
-                    <div className="itemCol"> 
+                <div className="container">
+                <div className="footer-top">
+                    <div className="footer-col">
+                        <Link href="/" title='Ancre to top'>
+                            <figure>
+                                <Image src="/images/logo.svg" alt="Travelian Logo Site" width={234} height={45} title='Travelian logo image'/>
+                            </figure>
+                        </Link>
                         <p>2024 © Developed by <Link href="https://andriamahay-irimanana.vercel.app/" target='_blank' className='copyRightLink'>IRIMANANA Henikaja Andriamahay </Link></p>
+                    </div>
+                    <div className="footer-col">
+                        <h3 className="title-h3">Menu</h3>
+                        <ul className='list-footer'>
+                            <li className="list-item">
+                                <Link href="/" className="list-link">Home</Link>
+                            </li>
+                            <li className="list-item">
+                                <Link href="/explore" className="list-link">Explore</Link>
+                            </li>
+                            <li className="list-item">
+                                <Link href="/travel" className="list-link">Travel</Link>
+                            </li>
+                            <li className="list-item">
+                                <Link href="/blog" className="list-link">Blog</Link>
+                            </li>
+                            <li className="list-item">
+                                <Link href="/pricing" className="list-link">Pricing</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <h3 className="title-h3">Information</h3>
+                        <ul className='list-footer'>
+                            <li className="list-item">
+                                <Link href="/contact" className="list-link">Destinations</Link>
+                            </li>
+                            <li className="list-item">
+                                <Link href="/contact" className="list-link">Supports</Link>
+                            </li>
+                            <li className="list-item">
+                                <Link href="/contact" className="list-link">Term & Conditions</Link>
+                            </li>
+                            <li className="list-item">
+                                <Link href="/contact" className="list-link">Privacy</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <h3 className="title-h3">Contact Info</h3>
+                        <ul className='list-footer'>
+                            <li className="list-item">
+                                <Link href="tel:" className="list-link">Destinations</Link>
+                            </li>
+                            <li className="list-item">
+                                <Link href="mailto:info@travellian.com" className="list-link">info@travellian.com</Link>
+                            </li>
+                            <li className="list-item">1245, New Yourk, USA</li>
+                        </ul>
+                    </div>
+                    <div className="footer-col">
+                        <h3 className="title-h3">Follow us on</h3>
+                        <ul className="list-ns">
+                            <li className="list-item">
+                                <Link href="#" className='list-link' target='_blank'><i className='icon icon-facebook'></i></Link>
+                            </li>
+                            <li className="list-item">
+                                <Link href="#" className='list-link' target='_blank'><i className='icon icon-pinterest'></i></Link>
+                            </li>
+                            <li className="list-item">
+                                <Link href="#" className='list-link' target='_blank'><i className='icon icon-instagram'></i></Link>
+                            </li>
+                            <li className="list-item">
+                                <Link href="#" className='list-link' target='_blank'><i className='icon icon-twitter'></i></Link>
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
