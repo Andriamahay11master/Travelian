faaba044ecd5363db8a13d8ccf90892819b1e6a1
Andriamahay11master <hirimanana@yahoo.fr>
2024-06-27 15:22:14 +0300

    add content banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,24 +32,28 @@ export default function Home() {
 
   const bannerList = [
     {
-      title: 'Start your unforgettable journey with us.',
-      desc: 'The best travel for your jouney begins now',
-      btn:'Discover',
-      linkBtn:'#',
-      srcImage:'/images/banner/tourism.jpg',
+      title: 'Discover the Beauty and Diversity of Switzerland',
+      desc: `Explore Switzerland's stunning landscapes, vibrant cities, and rich culture. 
+      From the majestic Alps to serene lakes, Switzerland offers natural beauty and endless adventures. 
+      Enjoy Swiss traditions, world-renowned cuisine, and charming villages. Plan your Swiss journey today!`,
+      btn:'Discover Swiss',
+      linkBtn:'https://www.myswitzerland.com/fr-fr/',
+      srcImage:'/images/banner/suisse.jpg',
       width: 1920,
       height: 1280,
-      altImage:''
+      altImage:'Tourism in Swiss'
     },
     {
-      title: 'Start your unforgettable journey with us.',
-      desc: 'The best travel for your jouney begins now',
-      btn:'Discover',
-      linkBtn:'#',
-      srcImage:'/images/banner/tourism.jpg',
+      title: 'Explore the Wonders of Norway',
+      desc: `Experience Norway's stunning fjords, majestic mountains, and vibrant cities. 
+      From the Northern Lights to picturesque coastal towns, Norway offers breathtaking natural 
+      beauty and rich cultural heritage. Enjoy outdoor adventures, explore Viking history, and savor local cuisine. Plan your Norwegian adventure today!`,
+      btn:'Explore Now',
+      linkBtn:'https://www.visitnorway.fr/',
+      srcImage:'/images/banner/norvege.jpg',
       width: 1920,
       height: 1280,
-      altImage:'Tourism 1'
+      altImage:'Tourism in Norvegian'
     }
   ]
 
