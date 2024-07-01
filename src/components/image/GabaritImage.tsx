8fd047f26431a0a4dd31ae2204e629e515ccb982
Andriamahay11master <hirimanana@yahoo.fr>
2024-07-01 16:27:28 +0300

    add CSS component image

---
+++ src/components/image/GabaritImage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './gabaritImage.scss';
+import Image from "next/image";
+
+interface GabaritImageProps {
+    href: string;
+    title?: string;
+    desc?: string;
+    srcImage: string;
+    width: number;
+    height: number;
+    altImage: string;
+}
+export default function GabaritImage({href ,title, desc, srcImage, width, height, altImage} : GabaritImageProps) {
+    return(
+        <a href={href} className="gabaritImage">
+            <Image className="gabaritImage-img" src={srcImage} width={width} height={height} alt={altImage} loading="lazy" title={altImage}/>
+            {title && <h3 className="title-h3 gabaritImage-title">{title}</h3>}
+            {desc && <div className="gabaritImage-desc-block"><i className="icon-map"></i><p className="gabaritImage-desc">{desc}</p></div>}
+        </a>
+    )
+}
