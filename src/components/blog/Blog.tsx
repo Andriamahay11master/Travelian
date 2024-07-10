60497024694300ecf932b589f999178062a3c3d2
Andriamahay11master <hirimanana@yahoo.fr>
2024-07-10 15:08:53 +0300

    init CSS component blog

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -21,9 +21,11 @@ export default function Blog({srcImage, wImage, hImage, altImage, title, desc, t
                 <Image src={srcImage} width={wImage} height={hImage} alt={altImage} loading="lazy" title={altImage}/>
             </div>
             <div className="blogItem-content">
-                <h3 className="blogItem-title">{title}</h3>
-                <p className='blogItem-desc'>{desc}</p>
-                <Link href={href} className='btn btn-primary' title='Blog link' target='_blank'>{txtButton}</Link>
+                <div className="blogItem-body">
+                    <h3 className="blogItem-title title-h3">{title}</h3>
+                    <p className='blogItem-desc'>{desc}</p>
+                    <Link href={href} className='btn btn-arrow btn-primary' title='Blog link' target='_blank'>{txtButton}</Link>
+                </div>
             </div>
         </div>
     );
