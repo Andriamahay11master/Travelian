5f3e53f90012e4ebff562ee354c6f73372a6735b
Andriamahay11master <hirimanana@yahoo.fr>
2024-07-12 09:03:14 +0300

    init offer component

---
+++ src/components/offer/Offer.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './offer.scss';
+import Link from 'next/link';
+import Image from 'next/image';
+import { OfferType } from '@/models/OfferType';
+
+export default function Offer({srcImage, altImage, wImage, hImage, title, note, desc, price, txtButton, hrefButton} : OfferType){
+    return (
+        <div className='offer'>
+            <div className="offer-top">
+                <Image src={srcImage} width={wImage} height={hImage} alt={altImage} loading="lazy" title={altImage}/>
+            </div>
+            <div className="offer-bottom">
+                <h3 className="offer-title">{title}</h3>
+                {note && (
+                    <div className='offer-note'>
+                        {[...Array(note)].map((_, index) => (
+                            <i key={index} className="icon-star"></i>
+                        ))}
+                    </div>
+                )}  
+                <p className="offer-desc">{desc}</p>
+                <div className="offer-details">
+                    <div className="offe-details-col">
+                        <p className="offer-price">From<span>{price}</span></p>
+                    </div>
+                    <div className="offe-details-col">
+                        <Link href={hrefButton} className='btn btn-primary' title='Offer link' target='_blank'>{txtButton}</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/models/OfferType.ts
@@ -0,0 +1,12 @@
+export type OfferType = {
+    srcImage: string;
+    altImage: string;
+    wImage: number;
+    hImage: number;
+    title: string;
+    note: number;
+    desc: string;
+    price: number;
+    txtButton: string;
+    hrefButton: string
+}
