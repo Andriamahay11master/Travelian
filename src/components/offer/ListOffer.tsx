603ec998f033783a6c8df3588ae243780996ca3a
Andriamahay11master <hirimanana@yahoo.fr>
2024-07-12 09:38:32 +0300

    carousel offer

---
+++ src/components/offer/ListOffer.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Offer from './Offer';
+import { OfferType } from '@/models/OfferType';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+interface ListOfferProps {
+    list: OfferType[]
+}
+export default function ListOffer({list} : ListOfferProps) {
+    const responsive = {
+        desktop: {
+          breakpoint: { max: 3000, min: 1200 },
+          items: 3
+        },
+        tablet: {
+          breakpoint: { max: 1200, min: 768 },
+          items: 2
+        },
+        mobile: {
+          breakpoint: { max: 768, min: 0 },
+          items: 1
+        }
+      };
+
+    return (
+        <Carousel className="carousel-list-full" responsive={responsive} transitionDuration={500} ssr={true} renderArrowsWhenDisabled={true}>
+            {list.map((item, index) => (
+                <Offer key={index} {...item}/>
+            ))}
+        </Carousel>
+    )
+}
